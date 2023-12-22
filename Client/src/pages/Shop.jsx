40d6b9132218878b64b197aa502795a4f9e30082
mr.Brian <brianprogrammer0@gmail.com>
2023-12-22 18:22:01 +0330

    Client Update ->  Displaying the status of receiving the products and displaying the message that the product are not found
    Client Update -> Create a table to display the list of orders

--- Client/src/pages/Shop.jsx
+++ Client/src/pages/Shop.jsx
@@ -1,30 +1,52 @@
 import { Row, Col } from 'react-bootstrap'
 
 import ProductItem from '../components/ProductItem'
-import { useEffect,useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getProducts } from '../data/items'
 
 function Shop() {
-   var [productList, setProductList] = useState([])
+  var [productList, setProductList] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
+  const [isReceived, setIsReceived] = useState(true);
+
   useEffect(() => {
-    getProducts().then((res)=>{
-    
-      setProductList(res)
+    getProducts().then((res) => {
+      setIsLoading(false);
+      if (res.msg) {
+        setIsReceived(false)
+      } else {
+        setProductList(res)
+      }
+
 
     })
-    
-  },[])
-  
+
+  }, [])
+
   return (
 
-    
-    <Row xs={1} md={2} lg={4} className='g-4'>
-      {productList.map((item) => (
-        <Col align='center' key={item.product_id}>
-          <ProductItem product={item} />
-        </Col>
-      ))}
-    </Row>
+
+    <div className='text-center mt-3'>
+      {isLoading ?
+        <div className="spinner-border text-light" role="status">
+          <span className="sr-only"></span></div>
+        : null}
+
+      {!isReceived ?
+        <div className="alert alert-danger" role="alert">
+          محصولی یافت نشد 
+        </div>
+        : null}
+
+      <Row xs={1} md={2} lg={4} className='g-4'>
+
+        {productList.map((item) => (
+          <Col align='center' key={item.product_id}>
+            <ProductItem product={item} />
+          </Col>
+        ))}
+      </Row>
+    </div>
 
 
   )
--- Client/src/pages/Success.jsx
+++ Client/src/pages/Success.jsx
@@ -4,6 +4,39 @@ function Success() {
   return (
     <div className='d-flex align-items-center justify-content-center flex-column my-4'>
       <h2>سفارش با موفقیت ثبت شد</h2>
+      <div className='mt-5 mb-5 w-50'>
+        <table className="table table table-dark table-striped  text-center">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td >Larry the Bird</td>
+              <td >Larry the Bird</td>
+
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
       <Link to='/' className='btn btn-light mt-2'>
         بازگشت به فروشگاه
       </Link>
